985719054162d0465fa59a2be8ac3ffdf536d0c3
RenatoCostaF <renatofurtado33@gmail.com>
2022-04-14 01:04:30 -0300

    first commit

---
+++ relogio/assets/js/main.js
@@ -0,0 +1,59 @@
+function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    });
+}
+
+
+const relogio = document.querySelector('.relogio');
+/* const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar'); */
+let segundos = 0;
+let timer;
+
+function iniciaRelogio() {
+    timer = setInterval(function () {
+        segundos++;
+        relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000);
+
+}
+/*PRIMEIRA FORMA DE FAZER*/ 
+document.addEventListener('click', function (e) {
+    const elemento = e.target;
+    if (elemento.classList.contains('iniciar')) {
+        relogio.classList.remove('pausado');
+        clearInterval(timer);
+        iniciaRelogio();
+    }
+    if (elemento.classList.contains('pausar')) {
+        relogio.classList.add('pausado');
+        clearInterval(timer);
+    }
+    if (elemento.classList.contains('zerar')) {
+        clearInterval(timer);
+        relogio.innerHTML = '00:00:00';
+        relogio.classList.remove('pausado');
+        segundos = 0;
+    }
+});
+
+
+/*SEGUNDA FORMA DE FAZER*/ 
+/* iniciar.addEventListener('click', function (event) {
+    relogio.classList.remove('.pausado');
+    clearInterval(timer);
+    iniciaRelogio();
+});
+pausar.addEventListener('click', function (event) {
+    relogio.classList.add('.pausado');
+    clearInterval(timer);
+});
+zerar.addEventListener('click', function (event) {
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00';
+    segundos = 0;
+}); */
